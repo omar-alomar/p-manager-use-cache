3c77e8804f331c32cb61062d03f4d14c34ee9c80
root <root@DESKTOP-4MHEEUD.localdomain>
2025-07-05 00:13:07 -0400

    initial commit

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,26 +1,45 @@
 import Link from "next/link"
 import { Skeleton, SkeletonButton } from "./Skeleton"
+import { getUser } from "@/db/users"
+import { notFound } from "next/navigation"
+
 
 export function ProjectCard({
   id,
   title,
+  client,
   body,
   apfo,
-  client
+  userId
 }: {
   id: number
   title: string
+  client: string
   body: string
   apfo: string
-  client: string
+  userId: number
 }) {
   return (
     <div className="card">
-      <div className="card-header">{title}</div>
-      <div>{apfo}</div>
+      
+      <div className="card-header">
+        <div className="header-left">
+      <h3 className="card-title">{title}</h3>
+      <div className="card-subtitle">Client: {client}</div>
+    </div>
+
+    <div className="header-right">
+      <div className="card-apfo">{apfo}</div>
+      <ProjectManagerInline userId={userId} />
+    </div>
+
+      </div>
+
+
       <div className="card-body">
         <div className="card-preview-text whitespace-pre-wrap">{body}</div>
       </div>
+
       <div className="card-footer">
         <Link className="btn" href={`/projects/${id}`}>
           View
@@ -29,6 +48,14 @@ export function ProjectCard({
     </div>
   )
 }
+
+async function ProjectManagerInline({ userId }: { userId: number }) {
+  const user = await getUser(userId)
+  if (!user) return null
+
+  return <div className="card-manager-inline">PM: {user.name}</div>
+}
+
 
 export function SkeletonProjectCard() {
   return (
@@ -50,3 +77,4 @@ export function SkeletonProjectCard() {
     </div>
   )
 }
+
